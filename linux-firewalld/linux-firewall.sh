d7dc63c0301b5936a2b7abfdeaee00365364e5fe
Mark Young <markusyou@gmail.com>
2020-06-22 16:56:35 -0600

    remove nfs entries

--- linux-firewalld/linux-firewall.sh
+++ linux-firewalld/linux-firewall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Install ansible and git ackage"
+echo "Install ansible and git package"
 echo "Use this password: mjolnir123"
 sudo yum -y install ansible git
 git clone https://github.com/markusyou/kodekloud-ansible.git
@@ -11,12 +11,10 @@ locahost ansible_ssh_user=tony ansible_ssh_pass="mjolnir123" ansible_ssh_key="/h
 [apps]
 stapp01 ansible_ssh_user=tony ansible_ssh_pass="Ir0nM@n" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub" ansible_become_user=root ansible_become_pass="Ir0nM@n"
 stapp02  ansible_ssh_user=steve ansible_ssh_pass="Am3ric@" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub"  ansible_become_user=root ansible_become_pass="Am3ric@"
-stapp03 ansible_ssh_user=banner ansible_ssh_pass="BigGr33n" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub"  ansible_become_user=root ansible_become_pass="BigGr33n" 
-[nfsserver]
-ststor01  ansible_ssh_user=natasha ansible_ssh_pass="Bl@kW" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub"  ansible_become_user=root ansible_become_pass="Bl@kW"' \
-> /home/thor/kodekloud-ansible/linux-nfs/hosts
-cd /home/thor/kodekloud-ansible/linux-nfs
+stapp03 ansible_ssh_user=banner ansible_ssh_pass="BigGr33n" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub"  ansible_become_user=root ansible_become_pass="BigGr33n" '\
+> /home/thor/kodekloud-ansible/linux-firewalld/hosts 
+cd /home/thor/kodekloud-ansible/linux-firewalld
 echo "Launch jump server ansible script."
 echo "ansible-playbook -i ./hosts jumpserver.yml"
 echo "Launch nfs server ansible script"
-echo "ansible-playbook -i ./hosts nfs-server.yml"
+echo "ansible-playbook -i ./hosts linux-firewalld.yml"
