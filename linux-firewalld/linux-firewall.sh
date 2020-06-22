697876497ed3f4246e3c3fc7c2137bdb0df36550
Mark Young <markusyou@gmail.com>
2020-06-22 16:08:13 -0600

    Inital setup

---
+++ linux-firewalld/linux-firewall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Install ansible and git ackage"
+echo "Use this password: mjolnir123"
+sudo yum -y install ansible git
+git clone https://github.com/markusyou/kodekloud-ansible.git
+cd 
+echo "Create hosts file in thor's homedir on jump server"
+echo '
+[jumphost]
+locahost ansible_ssh_user=tony ansible_ssh_pass="mjolnir123" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub" ansible_become_user=root ansible_become_pass="mjolnir123"
+[apps]
+stapp01 ansible_ssh_user=tony ansible_ssh_pass="Ir0nM@n" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub" ansible_become_user=root ansible_become_pass="Ir0nM@n"
+stapp02  ansible_ssh_user=steve ansible_ssh_pass="Am3ric@" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub"  ansible_become_user=root ansible_become_pass="Am3ric@"
+stapp03 ansible_ssh_user=banner ansible_ssh_pass="BigGr33n" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub"  ansible_become_user=root ansible_become_pass="BigGr33n" 
+[nfsserver]
+ststor01  ansible_ssh_user=natasha ansible_ssh_pass="Bl@kW" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub"  ansible_become_user=root ansible_become_pass="Bl@kW"' \
+> /home/thor/kodekloud-ansible/linux-nfs/hosts
+cd /home/thor/kodekloud-ansible/linux-nfs
+echo "Launch jump server ansible script."
+echo "ansible-playbook -i ./hosts jumpserver.yml"
+echo "Launch nfs server ansible script"
+echo "ansible-playbook -i ./hosts nfs-server.yml"
--- linux-firewalld/readme.txt
+++ linux-firewalld/readme.txt
@@ -10,3 +10,31 @@ c. All rules must be permanent.
 d. Zone should be public.
 
 e. If Apache or Nginx services aren't running already please make sure to start them.
+
+sudo yum install firewalld
+sudo yum install apache
+sudo yum install nginx
+sudo service enable firewalld
+sudo service enable apache
+sudo service enable nginx
+
+Configure firewalld 
+
+
+# Port 80 open (Ngnix incoming connections)
+firewall-cmd --permanent --zone=public --add-service=http 
+
+# Port 80 open (Ngnix incoming connections)
+firewall-cmd --permanent --zone=public --add-service=http 
+
+
+
+Load firewall rule changes
+sudo firewall-cmd --reload
+
+yum -y install nginx httpd firewalld
+systemctl restart nginx
+systemctl restart httpd
+systemctl restart firewalld
+sudo firewall-cmd --zone=public --add-service=http --permanent
+firewall-cmd --permanent --zone=public --add-rich-rule 'rule family="ipv4" source address="172.16.238.14" port port=8080 protocol=tcp accept'
