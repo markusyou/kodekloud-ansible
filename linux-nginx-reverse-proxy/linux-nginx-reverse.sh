197cf3e29bba4af4edca5a59b3c734cb99ba5234
Mark Young <markusyou@gmail.com>
2020-06-28 12:52:47 -0600

    Fix bash variable errorsa and add tasks to tasks.yml

--- linux-nginx-reverse-proxy/linux-nginx-reverse.sh
+++ linux-nginx-reverse-proxy/linux-nginx-reverse.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set project_name = "/home/thor/kodekloud-ansible"
-set project_path = "${project_name}/linux-nginx-reverse'
+project_name="/home/thor/kodekloud-ansible"
+project_path="${project_name}/linux-nginx-reverse-proxy"
 echo "Install ansible and git package"
 echo "Use this password: mjolnir123"
 sudo yum -y install ansible git
 git clone https://github.com/markusyou/kodekloud-ansible.git
-cd 
+cd
 echo "Create hosts file in thor's homedir on jump server"
 echo '
 [jumphost]
@@ -14,9 +14,10 @@ locahost ansible_ssh_user=tony ansible_ssh_pass="mjolnir123" ansible_ssh_key="/h
 stapp01 ansible_ssh_user=tony ansible_ssh_pass="Ir0nM@n" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub" ansible_become_user=root ansible_become_pass="Ir0nM@n"
 stapp02  ansible_ssh_user=steve ansible_ssh_pass="Am3ric@" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub"  ansible_become_user=root ansible_become_pass="Am3ric@"
 stapp03 ansible_ssh_user=banner ansible_ssh_pass="BigGr33n" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub"  ansible_become_user=root ansible_become_pass="BigGr33n" '\
-> ${ project_path }/hosts 
-cd "${ project_path }"
+> ${project_path}/hosts
+echo "Changing directory to ${project_path}"
+cd "${project_path}"
 echo "Launch jump server ansible script."
-ansible-playbook -i hosts jumoserver.yml
+ansible-playbook -i hosts jumpserver.yml
 echo "Launch project ansible script"
 ansible-playbook -i ./hosts ${project_name}.yml
--- linux-nginx-reverse-proxy/readme.txt
+++ linux-nginx-reverse-proxy/readme.txt
@@ -1,3 +1,21 @@
+Nautilus system admin's team is planning to deploy a front end application for their backup utility on Nautilus Backup Server, so that they can manage the backups of different websites from a graphical user interface. They have shared requirements to set up the same; please accomplish the tasks as per detail given below:
+
+
+a. Install Apache Server on Nautilus Backup Server and configure it to use 3000 port (do not bind it to 127.0.0.1 only, keep it default i.e let Apache listen on server's IP, hostname, localhost, 127.0.0.1 etc).
+
+b. Install Nginx webserver on Nautilus Backup Server and configure it to use 8093.
+
+c. Configure Nginx as a reverse proxy server for Apache.
+
+d. There is a sample index file /home/index.html on Jump Host, copy that file to Apache's document root.
+
+e. Make sure to start Apache and Nginx services.
+
+f. You can test final changes using curl command, e.g curl http://<backup server IP or Hostname>:8093.
+
+
+------------------------------
+
 # Redhat hat 7 solution
 access.redhat.com/solutions/1225423
 
