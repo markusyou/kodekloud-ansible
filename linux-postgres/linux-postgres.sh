7b342796e36856de283d0d3ce6632e68d7e6e7d6
markusyou <17938242+markusyou@users.noreply.github.com>
2020-07-23 16:39:57 -0600

    Update linux-postgres.sh
    FIx password for db server

--- linux-postgres/linux-postgres.sh
+++ linux-postgres/linux-postgres.sh
@@ -11,14 +11,12 @@ echo "Create hosts file in thor's homedir on jump server"
 echo '
 [jumphost]
 locahost ansible_ssh_user=thor ansible_ssh_pass="mjolnir123" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub" ansible_become_user=root ansible_become_pass="mjolnir123"
-[bk]
-stbkp01 ansible_ssh_user=clint ansible_ssh_pass="H@wk3y3" ansible_ssh_key="/home/clint/.ssh/id_rsa.pub" ansible_become_user=root ansible_become_pass="H@wk3y3" 
 [apps]
 stapp01 ansible_ssh_user=tony ansible_ssh_pass="Ir0nM@n" ansible_ssh_key="/home/clint/.ssh/id_rsa.pub" ansible_become_user=root ansible_become_pass="Ir0nM@n"  
 stapp02 ansible_ssh_user=steve ansible_ssh_pass="Am3ric@" ansible_ssh_key="/home/clint/.ssh/id_rsa.pub" ansible_become_user=root ansible_become_pass="Am3ric@"  
 stapp03 ansible_ssh_user=banner ansible_ssh_pass="BigGr33n" ansible_ssh_key="/home/clint/.ssh/id_rsa.pub" ansible_become_user=root ansible_become_pass="BigGr33n" 
 [db]
-stbkp01 ansible_ssh_user=peter ansible_ssh_pass="Sp!dy" ansible_ssh_key="/home/clint/.ssh/id_rsa.pub" ansible_become_user=root ansible_become_pass="H@wk3y3" ' \
+stbkp01 ansible_ssh_user=peter ansible_ssh_pass="Sp!dy" ansible_ssh_key="/home/clint/.ssh/id_rsa.pub" ansible_become_user=root ansible_become_pass="Sp!dy" ' \
 > ${project_path}/hosts
 echo "Changing directory to ${project_path}"
 cd "${project_path}"
