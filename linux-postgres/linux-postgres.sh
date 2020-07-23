844ea76c6012a8d551e7d9d19ef8e5dd6230a61d
markusyou <17938242+markusyou@users.noreply.github.com>
2020-07-23 16:45:38 -0600

    Update linux-postgres.sh
    correct db hostname

--- linux-postgres/linux-postgres.sh
+++ linux-postgres/linux-postgres.sh
@@ -16,7 +16,7 @@ stapp01 ansible_ssh_user=tony ansible_ssh_pass="Ir0nM@n" ansible_ssh_key="/home/
 stapp02 ansible_ssh_user=steve ansible_ssh_pass="Am3ric@" ansible_ssh_key="/home/clint/.ssh/id_rsa.pub" ansible_become_user=root ansible_become_pass="Am3ric@"  
 stapp03 ansible_ssh_user=banner ansible_ssh_pass="BigGr33n" ansible_ssh_key="/home/clint/.ssh/id_rsa.pub" ansible_become_user=root ansible_become_pass="BigGr33n" 
 [db]
-stbkp01 ansible_ssh_user=peter ansible_ssh_pass="Sp!dy" ansible_ssh_key="/home/clint/.ssh/id_rsa.pub" ansible_become_user=root ansible_become_pass="Sp!dy" ' \
+stdb01 ansible_ssh_user=peter ansible_ssh_pass="Sp!dy" ansible_ssh_key="/home/clint/.ssh/id_rsa.pub" ansible_become_user=root ansible_become_pass="Sp!dy" ' \
 > ${project_path}/hosts
 echo "Changing directory to ${project_path}"
 cd "${project_path}"
