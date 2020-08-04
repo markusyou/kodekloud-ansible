9c10e526d5350e263c81b5b7203caadd7f893af7
markusyou <17938242+markusyou@users.noreply.github.com>
2020-08-04 14:47:23 -0600

    Update linux-iptables.sh
    fix path issues

--- linux-iptables/linux-iptables.sh
+++ linux-iptables/linux-iptables.sh
@@ -12,9 +12,9 @@ locahost ansible_ssh_user=tony ansible_ssh_pass="mjolnir123" ansible_ssh_key="/h
 stapp01 ansible_ssh_user=tony ansible_ssh_pass="Ir0nM@n" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub" ansible_become_user=root ansible_become_pass="Ir0nM@n"
 stapp02  ansible_ssh_user=steve ansible_ssh_pass="Am3ric@" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub"  ansible_become_user=root ansible_become_pass="Am3ric@"
 stapp03 ansible_ssh_user=banner ansible_ssh_pass="BigGr33n" ansible_ssh_key="/home/thor/.ssh/id_rsa.pub"  ansible_become_user=root ansible_become_pass="BigGr33n" '\
-> /home/thor/kodekloud-ansible/linux-firewalld/hosts 
-cd /home/thor/kodekloud-ansible/linux-firewalld
+> /home/thor/kodekloud-ansible/linux-iptables/hosts 
+cd /home/thor/kodekloud-ansible/linux-iptables
 echo "Launch jump server ansible script."
 echo "ansible-playbook -i ./hosts jumpserver.yml"
 echo "Launch nfs server ansible script"
-echo "ansible-playbook -i ./hosts linux-firewall.yml"
+echo "ansible-playbook -i ./hosts linux-iptables.yml"
